d3f10e9456ead786a32de4fdebe1de8122da014d
Nilesh Ghule <nilesh@sunbeaminfo.com>
2023-06-30 15:14:25 +0530

    multiply

--- demo2.py
+++ demo2.py
@@ -2,8 +2,14 @@
 def subtract(a,b):
 	return a - b
 
+def multiply(a,b):
+	return a * b
+
 n1 = 10
 n2 = 4
 n3 = subtract(n1, n2)
 print("subtract", n3)
+
+n3 = multiply(n1, n2)
+print("multiply", n3)
 
